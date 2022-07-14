331e8e1f120eade745df7846d225b6654187af26
NeverMind109 <vladgladkih123@gmail.com>
2022-07-14 19:17:19 +0300

    Created: Added Popular Goods Section

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -3,6 +3,7 @@ Swiper.use([Navigation, Pagination, Thumbs]);
 
 const casualBigImage = document.querySelector("#casualClothesSlider");
 const casualClothesThumbs = document.querySelector("#casualClothesThumbs");
+const popularList = document.querySelector("#popularList");
 
 const casualClothesThumbsSlider = new Swiper(casualClothesThumbs, {
   spaceBetween: 10,
@@ -23,5 +24,16 @@ const casualClothesSlider = new Swiper(casualBigImage, {
 
   thumbs: {
     swiper: casualClothesThumbsSlider,
+  },
+});
+
+const popularListSlider = new Swiper(popularList, {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  loop: true,
+
+  navigation: {
+    nextEl: ".popular-goods__list-next",
+    prevEl: ".popular-goods__list-prev",
   },
 });
